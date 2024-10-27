684810fff047ad5c710d84af198c1f77f69590f7
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-10-27 18:30:52 +0200

    Add interfaces for repositories

---
+++ GoShop.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Entities;
+using Library.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<List<CategoryEntity>> GetAllAsync(CategoryFiltersModel model, CancellationToken cancellationToken);
+    Task<int> GetCountByFiltersAsync(CategoryFiltersModel model, CancellationToken cancellationToken);
+}
