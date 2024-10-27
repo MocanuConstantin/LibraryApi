12e137c4c028000dc5a7242bf20e55541efc3991
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-10-27 17:57:01 +0200

    Adding LibraryDb using EF Core

---
+++ GoShop.Data/LibraryDb.cs
@@ -0,0 +1,18 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data;
+
+public class LibraryDb : DbContext
+{
+    public DbSet<BookEntity> BookEntities {  get; set; }
+    public DbSet<AuthorEntity> AuthorEntities {  get; set; }
+    public DbSet<CategoryEntity> CategoryEntities {  get; set; }
+    public LibraryDb(DbContextOptions<LibraryDb> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
